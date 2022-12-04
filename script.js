ff3c65f4e6424abdffda59600954e2ebf5d832b5
dev-tura <adelajatura@gmail.com>
2022-12-04 23:22:39 +0100

    javascript dom

---
+++ script.js
@@ -0,0 +1,12 @@
+(function () {
+  // Bind Click event to the drop down navigation button
+  document.querySelector(ul).addEventListener(
+    "click",
+    function () {
+      /*  Toggle the CSS closed class which reduces the height of the UL thus 
+        hiding all LI apart from the first */
+      this.parentNode.parentNode.classList.toggle("closed");
+    },
+    false
+  );
+})();
